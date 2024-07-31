7bb04e0d801f28acf48c9f6fe56d59d6fbaacdd0
Riiz0 <shawnrizo@hotmail.com>
2024-07-31 18:46:39 -0400

    (Chore) Comment out error code for Create.tsx file

--- frontend/components/nftmarketplace/createpage/create.tsx
+++ frontend/components/nftmarketplace/createpage/create.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useState } from 'react'
-import { abi } from '../../../lib/abi/FactoryNFTContractABI';
-import { useWriteContract } from 'wagmi';
+// import { useState } from 'react'
+// import { abi } from '../../../lib/abi/FactoryNFTContractABI';
+// import { useWriteContract } from 'wagmi';
 import Header from '@/components/pagesHeader';
 
 export default function Create() {
